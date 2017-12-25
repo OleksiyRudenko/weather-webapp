e2827a329e02d3207a1d67bacc285b863ac97c49
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-12-25 15:15:41 +0200

    Add unit switch element click handler

--- src/js/controller/unitswitchcontroller.js
+++ src/js/controller/unitswitchcontroller.js
@@ -8,5 +8,15 @@ class UnitSwitchController {
   constructor(settingsService, unitSwitchElId) {
     this._settingsService = settingsService;
     this._unitSwitchEl = document.getElementById(unitSwitchElId);
+    this._unitSwitchEl.onclick = this.onClick.bind(this);
+    this._unitSwitchEl.ontouchstart = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    this._settingsService.switchUnits();
+    const list = this._unitSwitchEl.children;
+    for (let el of list) {
+      el.classList.toggle('active');
+    }
   }
 }
--- src/js/service/settingsservice.js
+++ src/js/service/settingsservice.js
@@ -7,5 +7,16 @@ class SettingsService {
    */
   constructor(appConfig) {
     this._appConfig = appConfig;
+    this._settings = {
+      Units: 'metric',
+    };
+  }
+
+  get units() { return this._settings.Units; }
+  setImperial() { this._settings.Units = 'imperial'; }
+  setMetric() { this._settings.Units = 'metric'; }
+
+  switchUnits() {
+    this._settings.Units = (this._settings.Units === 'metric') ? 'imperial' : 'metric';
   }
 }
