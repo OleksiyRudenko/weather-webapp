2f5475d07844ba853963b28775562a9e05388990
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-12-26 00:24:40 +0200

    Add city list loading

--- src/js/service/citylistservice.js
+++ src/js/service/citylistservice.js
@@ -7,5 +7,25 @@ class CityListService {
    */
   constructor(appConfig) {
     this._appConfig = appConfig;
+    this._cityList = []; // id: , name: 'cityname, country'
+
+    this.loadSource();
+  }
+
+  loadSource() {
+    fetch(window.location.protocol + "//" + window.location.host + "/" + this._appConfig.cityList, {method: 'get'})
+      .then(response => {
+        if (response.ok)
+         return response.json();
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        data.map(entry => {
+          this._cityList.push({id: entry.id, name:entry.name + ', ' + entry.country});
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 }
