28057915f5b8445c14d1bdd258b68cf96c14820b
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-01-02 12:48:02 +0200

    Update storage config

--- src/js/config.js
+++ src/js/config.js
@@ -8,7 +8,41 @@ const appConfig = {
   },
   cityList: 'assets/city.list.json',
   storage: {
-    dbName: 'weatherapp',
-    storeNames: ['settings', 'cities', 'favcity', 'cityhistory', 'weather'],
+    dbName: 'weatherapp-rdnk',
+    dbVersion: 1,
+    store: [
+      // [0]: upgrade to v.1
+      [
+        {
+          storeName: 'settings',
+          keyPath: {keyPath: 'option'},
+          fields: ['option', 'value'], // option = {Units|...}; Units={metric|imperial}
+        },
+        {
+          storeName: 'cities',
+          keyPath: {keyPath: 'id'},
+          fields: ['id', 'name', 'nameUC'], // city name, city name uppercase
+          index: [['nameUC']], // indices; when single literal then index name == index key name
+        },
+        {
+          storeName: 'favcity',
+          keyPath: {keyPath: 'id'},
+          fields: ['id', 'name', 'nameUC'], // city name, city name uppercase
+          index: [['nameUC']],
+        },
+        {
+          storeName: 'cityhistory',
+          keyPath: {keyPath: 'id'},
+          fields: ['id', 'name', 'nameUC', 'lastQueried', 'queryCount'], // city name, city name uppercase
+          index: [['nameUC'], ['lastQueried'], ['queryCount']],
+        },
+      ],
+      // [1]: upgrade to v.2
+      [
+        {
+          storeName: 'weather',
+        },
+      ],
+    ], // store
   },
 };
