98ac0178024bbc726b83e5896e011c1f39547e44
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-01-03 00:12:40 +0200

    Add UpdateStorage method

--- src/js/service/settingsservice.js
+++ src/js/service/settingsservice.js
@@ -7,6 +7,7 @@ class SettingsService {
    * @param {object} storageService - storage service
    */
   constructor(appConfig, storageService) {
+    this._storeName = 'settings';
     this._appConfig = appConfig;
     this._storageService = storageService;
     this._settings = {
@@ -20,5 +21,25 @@ class SettingsService {
 
   switchUnits() {
     this._settings.Units = (this._settings.Units === 'metric') ? 'imperial' : 'metric';
+  }
+
+  /**
+   * Update settings in storage
+   */
+  updateStorage() {
+    const data = {
+      option: 'Units',
+      value: this._settings.Units,
+    };
+    this._storageService.put(this._storeName,data).then(()=>{
+      console.log('Settings store updated')
+    });
+  }
+
+  /**
+   * Load settings from store
+   */
+  loadSettings() {
+    // this._storageService.getAll();
   }
 }
