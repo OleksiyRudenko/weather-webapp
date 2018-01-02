a15ff7ae4d53116736f79de8f7e502d142feca90
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-01-02 18:12:12 +0200

    Add put initial 10 entries from citylist.json to idb

--- src/js/service/citylistservice.js
+++ src/js/service/citylistservice.js
@@ -7,18 +7,36 @@ class CityListService {
    * @param {object} storageService - storage service
    */
   constructor(appConfig, storageService) {
+    this._storeName = 'cities';
     this._appConfig = appConfig;
     this._storageService = storageService;
-    this._cityList = []; // id: , name: 'cityname, country'
+    // this._cityList = []; // id: , name: 'cityname, country'
 
-    this.loadSource();
+    this.updateStorage();
+  }
+
+  /**
+   * Update idb storage if required
+   */
+  updateStorage() {
+    this._storageService.storeCount(this._storeName).then(count => {
+      if (!count) {
+        this.loadSource().then(cityList => {
+          cityList = cityList.slice(0,10);
+          this._storageService.put(this._storeName, cityList).then(()=>{
+            console.log('idb.cities updated.');
+          });
+        });
+      }
+      console.log('CityListService.updateStorage(cities).count==' + count);
+    });
   }
 
   /**
    * Import data (cities list) from external resource and customize it as appropriate
    */
   loadSource() {
-    fetch(this._appConfig.baseUrl + this._appConfig.cityList, {method: 'get'})
+    return fetch(this._appConfig.baseUrl + this._appConfig.cityList, {method: 'get'})
       .then(response => {
         if (response.ok)
          return response.json();
@@ -28,7 +46,7 @@ class CityListService {
         let list = data.map(entry =>
           ({id: entry.id, name:entry.name + ', ' + entry.country, nameUC: (entry.name + ', ' + entry.country).toUpperCase()})
         ).filter(el => /^[^\d\W]/.test(el.name));
-        this._cityList = list.sort((a,b) => a.nameUC < b.nameUC ? -1 : 1 );
+        return list.sort((a,b) => a.nameUC < b.nameUC ? -1 : 1 );
       })
       .catch(error => {
         console.error(error);
