f89d41235b9c5ef02ae7418ce6ea021175fe3c5e
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-01-03 19:07:31 +0200

    docs(helpers): update JSDoc

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,3 +1,14 @@
+/**
+ * @callback EventCallback
+ * @param {event} e - event data
+ */
+
+/**
+ * Attach mouse-click and touch event handlers to an HTML Element.
+ * @param {Element} htmlElement - The HTML Element to attach event handler to
+ * @param {EventCallback} eventHandler - The callback that handles the response
+ * @param {object} scope - scope to bind the callback to
+ */
 function attachOnClickEvent(htmlElement, eventHandler, scope) {
   htmlElement.onclick = eventHandler.bind(scope);
   htmlElement.ontouchstart = eventHandler.bind(scope);
