0e0ed21d12c5088c1913eef9786c54a37e91d4b5
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-01-08 10:52:56 +0200

    feat(weather): Add current weather data extraction

--- src/js/controller/weathercontroller.js
+++ src/js/controller/weathercontroller.js
@@ -21,11 +21,35 @@ class WeatherController {
     elProgressSpinner.classList.add('loader-visible');
     weatherData.then(data => {
       elProgressSpinner.classList.remove('loader-visible');
+      data = this.extractWeatherDataCurrent(data);
       this._element.today.innerHTML = 'TODAY: <pre>' + JSON.stringify(data, null, 2) + '</pre>';
     }).catch(error => {
       elProgressSpinner.classList.remove('loader-visible');
       this._element.today.innerText = 'Today: ' + error;
     });
+  }
+
+  /**
+   * Extracts required data from API call response
+   * @param {Object} src - API fetch data
+   */
+  extractWeatherDataCurrent(src) {
+    return {
+      dt: src.dt,
+      geocity: src.name,
+      geocountry: src.sys.country,
+      geolat: src.coord.lat,
+      geolon: src.coord.lon,
+      descr: src.weather.main,
+      descrDetails: src.weather.description,
+      descrIcon: src.weather.icon,
+      temp: src.main.temp,
+      pressure: src.main.pressure,
+      humidity: src.main.humidity,
+      windSpeed: src.wind.speed,
+      windAzimuth: src.wind.deg,
+      clouds: src.clouds.all,
+    };
   }
 
   /**
