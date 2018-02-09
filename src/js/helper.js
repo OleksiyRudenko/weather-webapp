42c94c045917d3ebd3926607cc52d5e2bf9534d0
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-02-09 09:13:37 +0200

    feat(helper): Add plain object detector

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,3 +1,28 @@
+/*  Function to test if an object is a plain object, i.e. is constructed
+**  by the built-in Object constructor and inherits directly from Object.prototype
+**  or null. Some built-in objects pass the test, e.g. Math which is a plain object
+**  and some host or exotic objects may pass also.
+**
+**  @param {} obj - value to test
+**  @returns {Boolean} true if passes tests, false otherwise
+**  Source: https://stackoverflow.com/questions/5876332/how-can-i-differentiate-between-an-object-literal-other-javascript-objects
+*/
+export const isPlainObject = (obj) => {
+  // Basic check for Type object that's not null
+  if (typeof obj === 'object' && obj !== null) {
+    // If Object.getPrototypeOf supported, use it
+    if (typeof Object.getPrototypeOf === 'function') {
+      const proto = Object.getPrototypeOf(obj);
+      return proto === Object.prototype || proto === null;
+    }
+    // Otherwise, use internal class
+    // This should be reliable as if getPrototypeOf not supported, is pre-ES5
+    return Object.prototype.toString.call(obj) === '[object Object]';
+  }
+  // Not an object
+  return false;
+};
+
 /**
  * @callback EventCallback
  * @param {event} e - event data
