c9d908aaecf93203aaa3c114b4695537f79b9b8b
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-02-09 18:41:17 +0200

    feat(UnitSwitchController): Migrate to framework model

--- src/js/controller/unitswitchcontroller.js
+++ src/js/controller/unitswitchcontroller.js
@@ -1,20 +1,29 @@
-import * as helper from './../helper.js';
+import AppUiControllerComponent from './../framework/appuicontrollercomponent.js';
+import SettingsService from './../service/settingsservice.js';
 
 /** Class representing unit switch controller. */
-export default class UnitSwitchController {
+export default class UnitSwitchController extends AppUiControllerComponent {
   /**
    * Create unit switch controller.
    * @constructor
-   * @param {object} appConfig - city list service
-   * @param {CityInputController} cityInputController
-   * @param {SettingsService} settingsService - settings service
    */
-  constructor(appConfig, cityInputController, settingsService) {
-    this._config = appConfig.unitSwitch;
-    this._cityInputController = cityInputController;
-    this._settingsService = settingsService;
-    this._unitSwitchEl = document.getElementById(this._config.container);
-    helper.attachOnClickEvent(this._unitSwitchEl, this.onClick, this);
+  constructor() {
+    super();
+    this.dependencies = {
+      Services: {
+        SettingsService: 'SettingsService',
+      },
+      UiControllers: {
+        CityInputController: 'CityInputController',
+      }
+    };
+  }
+
+  /**
+   * Runs main job
+   */
+  run() {
+    this.attachOnClickHandler(this.uiElements.container, this.onClick);
     this.render();
   }
 
@@ -22,16 +31,13 @@ export default class UnitSwitchController {
    * Render UI
    */
   render() {
-    this._settingsService.settingsPromise.then( value => {
-      const unit = this._settingsService.units;
-      const unitElementsList = this._unitSwitchEl.children;
+    this.dependencies.SettingsService.settingsPromise.then( value => {
+      const unit = this.dependencies.SettingsService.units;
+      const unitElementsList = this.uiElements.container.children;
       let unit0 = unitElementsList[0], unit1 = unitElementsList[1];
       if (unit === 'imperial') {
         [unit1, unit0] = [unit0, unit1];
       }
-      /* console.log(unit);
-      console.log(unit0);
-      console.log(unit1); */
       unit0.classList.add('active');
       unit1.classList.remove('active');
     });
@@ -42,12 +48,12 @@ export default class UnitSwitchController {
    * @param {object} e - click event
    */
   onClick(e) {
-    this._settingsService.switchUnits();
-    const unitElementsList = this._unitSwitchEl.children;
+    this.dependencies.SettingsService.switchUnits();
+    const unitElementsList = this.uiElements.container.children;
     for (let el of unitElementsList) {
       el.classList.toggle('active');
     }
-    this._cityInputController.actionSearch();
-    this._cityInputController.focus();
+    this.dependencies.CityInputController.actionSearch();
+    this.dependencies.CityInputController.focus();
   }
 }
