de24d6c2268598c52e098db55b40490a8ffd289b
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-02-11 16:32:28 +0200

    feat(mood): Add mood imagery load dep on weather conditions, time of the day, season

--- src/js/controller/moodcontroller.js
+++ src/js/controller/moodcontroller.js
@@ -1,5 +1,4 @@
 import AppUiControllerComponent from "../framework/appuicontrollercomponent.js";
-import {imagery} from "../../assets/mood/imagery.js";
 
 /**
  * Class that control app general media
@@ -7,12 +6,45 @@ import {imagery} from "../../assets/mood/imagery.js";
 export default class MoodController extends AppUiControllerComponent {
   constructor() {
     super();
-    this.config = {};
+    this.config = {
+      mood: 'mood',
+    };
     this.dependencies = {};
   }
 
   run() {
     this.debugThisClassName('run');
-    console.log(imagery);
+  }
+
+  renderMood(date, geolat, verboseConditions) {
+    this.debugThisClassName('renderMood');
+    console.log(this.uiElements);
+    console.log(date);
+    console.log(verboseConditions);
+    const season = this.getSeason(date, geolat);
+    const img = this.getMoodImage(verboseConditions.conditions, verboseConditions.tod, season);
+    console.log(img);
+    this.uiElements.container.style.backgroundImage = "url('" + img + "')";
+  }
+
+  getMoodImage(conditions, tod, season) {
+    if (conditions === 'unknown') conditions = 'clearSky';
+    return this.config.mood.imagery[conditions][tod][season];
+  }
+
+  getSeason(date, geolat) {
+    const month = date.getMonth() + 1;
+    let season = month < 4 ? 'winter'
+      : month < 6 ? 'spring'
+        : month < 9 ? 'summer'
+          : month < 12 ? 'autumn'
+            : 'winter';
+    // invert season for Southern hemisphere
+    if (geolat < 0) {
+      season = season === 'winter' ? 'summer' : season === 'summer' ? 'winter' : season;
+    }
+    if (geolat > 65 || geolat < -60) season = 'winter';
+    if (geolat <=30 && geolat >-30) season = 'summer';
+    return season;
   }
 }
