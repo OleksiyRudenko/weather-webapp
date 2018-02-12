99d4565473691c2c0a8b14740d5f42d0064a4f54
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-02-12 14:50:03 +0200

    feat(CityListService): Improve error catching

--- src/js/service/citylistservice.js
+++ src/js/service/citylistservice.js
@@ -39,15 +39,22 @@ export default class CityListService extends AppServiceComponent {
   updateStorage() {
     this.dependencies.Services.StorageService.storeCount(this._storeName).then(count => {
       if (!count) {
-        this.loadSource().then(cityList => {
-          console.log(cityList.length + ' entries in the city list.');
-          cityList = cityList.slice(0, 1234);
-          this.dependencies.Services.StorageService.put(this._storeName, cityList).then(()=>{
-            console.log('idb.cities updated.');
+        this.loadSource()
+          .then(cityList => {
+            console.log(cityList.length + ' entries in the city list.');
+            const limit = 1234;
+            cityList = cityList.slice(0, limit);
+            this.dependencies.Services.StorageService.put(this._storeName, cityList).then(()=>{
+              console.log('idb.cities updated.');
+              return cityList.length;
+            }).then( entriesCount => {
+              console.log('CityListService.updateStorage(cities).count==' + entriesCount);
+            });
+          })
+          .catch(error => {
+            console.log(error);
           });
-        });
       }
-      console.log('CityListService.updateStorage(cities).count==' + count);
     });
   }
 
@@ -57,18 +64,26 @@ export default class CityListService extends AppServiceComponent {
   loadSource() {
     return fetch(this.config.baseUrl + this.config.cityList, {method: 'get'})
       .then(response => {
-        if (response.ok)
-         return response.json();
-        throw new Error('Network response was not ok.');
+        this.debugThisClassName('loadSource');
+        // console.log(response);
+        if (response.ok) {
+          const contentType = response.headers.get("content-type");
+          if (contentType && contentType.includes('application/json'))
+            return response.json();
+          throw new TypeError('JSON expected, ' + contentType + ' instead arrived');
+        }
+        throw new Error('Network response was not ok:' + response.error);
       })
       .then(data => {
+        // console.log(data);
         let list = data.map(entry =>
           ({id: entry.id, name:entry.name + ', ' + entry.country, nameUC: (entry.name + ', ' + entry.country).toUpperCase()})
         ).filter(el => /^[^\d\W]/.test(el.name));
         return list.sort((a,b) => a.nameUC < b.nameUC ? -1 : 1 );
       })
       .catch(error => {
-        console.error(error);
+        console.log(error);
+        throw new Error(error);
       });
   }
 }
