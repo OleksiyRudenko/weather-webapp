edb0089c50b93bb83fc2c6c513cdff13a989eb64
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-02-13 21:25:42 +0200

    feat(favcity): Setup favcity service

--- src/js/service/favcityservice.js
+++ src/js/service/favcityservice.js
@@ -7,6 +7,7 @@ export default class FavCityService extends AppServiceComponent {
    */
   constructor() {
     super();
+    this._storeName = 'favcity';
   }
 
   /* === Public methods === */
@@ -20,4 +21,28 @@ export default class FavCityService extends AppServiceComponent {
 
   /* === Private methods : SECONDARY === */
 
+  /**
+   * Add favourite city item to the storage
+   * @param {Object} item {name:cityname}
+   */
+  addEntry(item) {
+    // update item content
+    item.nameUC = item.name.toUpperCase();
+    // store item
+    this.dependencies.Services.StorageService.put(this._storeName,item);
+  }
+
+  /**
+   * Get {limit} last favourite cities items
+   * @param {number} limit=20 - Search limit
+   * @returns {Promise} Ordered list of citynames
+   */
+  getItems(limit=20) {
+    return this.dependencies.Services.StorageService.getAll(this._storeName).then(items => {
+      items.sort((a,b) => b.nameUC > a.nameUC);
+      items = items.slice(0,limit).map(item => item.name);
+      items.sort();
+      return items;
+    });
+  }
 }
